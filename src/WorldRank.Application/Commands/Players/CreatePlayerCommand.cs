89d11e5e14ef743ca64f5de4365184c5ad146ac6
ntua-el21068 <el21068@mail.ntua.gr>
2026-07-15 00:49:42 +0300

    Upgrade to net10, add MediatR + Autofac packages

---
+++ src/WorldRank.Application/Commands/Players/CreatePlayerCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldRank.Application.Commands.Players
+{
+    internal class CreatePlayerCommand
+    {
+    }
+}
