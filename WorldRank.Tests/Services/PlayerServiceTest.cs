2723325af34153ae2dac05b3b23e0c6530a39320
ntua-el21068 <el21068@mail.ntua.gr>
2026-07-17 14:56:13 +0300

    Added Tests project, started implementing WalletTests and PlayerServiceTests

---
+++ WorldRank.Tests/Services/PlayerServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Moq;
+using System.Collections.Generic;
+using System.Text;
+using WorldRank.Application.Caching;
+using WorldRank.Application.Repositories;
+using WorldRank.Application.Services;
+using WorldRank.Domain.Entities;
+
+namespace WorldRank.Tests.Services
+{
+    public class PlayerServiceTest
+    {
+        private readonly Mock<IPlayerRepository> _playerRepositoryMock = new();
+        private readonly Mock<ICache> _cacheMock = new();
+        private PlayerService _sut;
+
+        public PlayerServiceTest()
+        {
+            _sut = new(_playerRepositoryMock.Object, _cacheMock.Object);
+        }
+
+       
+
+        [Fact]
+        public async Task GetPlayerById_IdExists_ReturnsPlayer()
+        {
+            // Arrange
+            int id = 1;
+            var expectedPlayer = new Player(id, "Maria");
+
+            _cacheMock
+                .Setup(c => c.TryGet(It.IsAny<string>(), out It.Ref<Player?>.IsAny))
+                .Returns(false);
+
+            _playerRepositoryMock
+                .Setup(r => r.FindPlayerAsync(id, It.IsAny<CancellationToken>()))
+                .ReturnsAsync(expectedPlayer);
+
+            // Act
+            var player = await _sut.GetPlayerById(id, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(player);
+            Assert.Equal("Maria", player.Name);
+            Assert.Equal(id, player.Id);
+        }
+
+
+
+    }
+}
